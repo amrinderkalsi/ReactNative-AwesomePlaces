acde12d0ca2732a1c9e860fac925d43e1894b794
DESKTOP-80P1N95\Amrinder Kalsi <amrinder.kalsi@agnition.ca>
2019-05-15 10:22:33 -0400

    Added Vector Icone and First Commit

---
+++ AwesomeProject/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import { Provider } from 'react-redux';
+import App from './App';
+import configureStore from './src/store/configureStore';
+import {name as appName} from './app.json';
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+
+AppRegistry.registerComponent(appName, () => RNRedux);
