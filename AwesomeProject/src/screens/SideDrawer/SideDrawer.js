8cbe7d16cc76ae52c117caa048819f68870d39e7
DESKTOP-80P1N95\Amrinder Kalsi <amrinder.kalsi@agnition.ca>
2019-05-16 10:51:29 -0400

    Designed SideDrawer

--- AwesomeProject/src/screens/SideDrawer/SideDrawer.js
+++ AwesomeProject/src/screens/SideDrawer/SideDrawer.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
 
 class SideDrawer extends Component {
     render() {
         return(
             <View style={[styles.container, {width: Dimensions.get("window").width * 0.8}]}>
-                <Text>SideDrawer</Text>
+                <TouchableOpacity>
+                    <View style={styles.drawerItem}>
+                        <Icon name="ios-log-out" size={30} color={"black"} style={styles.drawerItemIcon} />
+                        <Text style={styles.drawerItemText}>Sign Out</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 22,
+        paddingTop: 30,
         backgroundColor: "white",
         flex: 1
+    },
+    drawerItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+        backgroundColor: "#eee"
+    },
+    drawerItemIcon: {
+        marginRight: 10
+    },
+    drawerItemText: {
+        color: "black"
     }
 });
 export default SideDrawer;
